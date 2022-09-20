63918fc88a816e902bbd2de4c43cdcea07657725
Yo-Tina Verbraecken <64355830+iheartcore@users.noreply.github.com>
2022-09-20 17:09:53 +0200

    Scroll to Top with Vue Router
    Using  a nice technique where we tell Vue Router to scroll the page to the top on certain navigations.

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,16 @@ import routes from './routes'
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
+  scrollBehavior(to) {
+    const scroll = {}
+    if (to.meta.toTop) {
+      scroll.top = 0
+    }
+    if (to.meta.smoothScroll) {
+      scroll.behavior = 'smooth'
+    }
+    return scroll
+  },
 })
 
 export default router
--- src/router/routes/index.ts
+++ src/router/routes/index.ts
@@ -16,6 +16,7 @@ const routes = [
     path: '/me',
     name: 'Profile',
     component: Profile,
+    meta: { toTop: true, smoothScroll: true },
   },
   {
     path: '/me/edit',
