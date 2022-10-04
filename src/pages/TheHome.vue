6cf5fa9b015b72960a770193d4ac1a3ca61e9517
Yo-Tina Verbraecken <64355830+iheartcore@users.noreply.github.com>
2022-10-04 16:44:39 +0200

    Hiding Content Until Data is Fully Loaded
    We improve user experience by removing the incremental showing, hiding all page content until all the data is available. In the Vue.js created hook, we're able to wait for all the data to finish loading and set a ready data property to true that conditionally show/hides the markup in the template.

--- src/pages/TheHome.vue
+++ src/pages/TheHome.vue
@@ -3,6 +3,11 @@
   import { allStore } from '@/stores'
 
   export default {
+    data() {
+      return {
+        ready: false,
+      }
+    },
     computed: {
       ...mapState(allStore.categoryStore, {
         categories: (store) => store.categories,
@@ -16,13 +21,16 @@
       const forumIds = categories.map((category) => category.forums).flat()
       const forums = await allStore.forumStore().fetchForums({ ids: forumIds })
       const threadIds = forums.map((forum) => forum.threads).flat()
-      allStore.threadStore().fetchThreads({ ids: threadIds })
+      await allStore.threadStore().fetchThreads({ ids: threadIds })
+      this.ready = true
     },
   }
 </script>
 
 <template>
-  <h1 class="push-top">Welcome to the Forum</h1>
-  <CategoryList :categories="categories" />
-  <ThreadList :threads="threads" />
+  <div v-if="ready" class="container">
+    <h1 class="push-top">Welcome to the Forum</h1>
+    <CategoryList :categories="categories" />
+    <ThreadList :threads="threads" />
+  </div>
 </template>
